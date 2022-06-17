b108239bbe7df7f63dc4a7640e491ceea723fc28
Veidz <joaovictorveidz@gmail.com>
2022-06-17 10:36:43 -0300

    refactor: challenge 03

--- challenges/challenge03.py
+++ challenges/challenge03.py
@@ -1,7 +1,8 @@
 # Create a script that reads two numbers and shows the sum of them.
-num1 = input('Type a number: ')
-num2 = input('Type another one: ')
+num1 = int(input('Type a number: '))
+num2 = int(input('Type another one: '))
 
-sum = int(num1) + int(num2)
+sum = num1 + num2
 
-print('The sum of', num1 + ' + ' + num2, 'is', sum)
+# print('The sum of', num1 + ' + ' + num2, 'is', sum)
+print('The sum of {} + {} is {}'.format(num1, num2, sum))
