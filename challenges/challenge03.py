f2d45d43534ffbf4a27cc1ac759c11a07a318d3e
Veidz <joaovictorveidz@gmail.com>
2022-06-16 21:14:40 -0300

    feat: challenge 03

---
+++ challenges/challenge03.py
@@ -0,0 +1,7 @@
+# Create a script that reads two numbers and shows the sum of them.
+num1 = input('Type a number: ')
+num2 = input('Type another one: ')
+
+sum = int(num1) + int(num2)
+
+print('The sum of', num1 + ' + ' + num2, 'is', sum)
