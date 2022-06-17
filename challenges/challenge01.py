661f87bffd27d82750d79ec170211a25acf33b24
Veidz <joaovictorveidz@gmail.com>
2022-06-16 21:14:56 -0300

    fix: challenge 01 typo

--- challenges/challenge01.py
+++ challenges/challenge01.py
@@ -1,3 +1,3 @@
-# Create a script that read the user name and shows a greetings message according to the entered value.
+# Create a script that reads the user name and shows a greetings message according to the entered value.
 name = input('What is your name? ')
 print('Greetings', name + ',', 'nice to meet you!');
