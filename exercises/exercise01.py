50c86af63721c333e7bcf3d3d116dd686178345d
Veidz <joaovictorveidz@gmail.com>
2022-06-17 10:53:22 -0300

    chore: remove exercises folder

--- exercises/exercise01.py
+++
@@ -1,2 +0,0 @@
-# Create a script that shows "Hello, World!".
-print("Hello, World!")
--- exercises/exercise02.py
+++
@@ -1,4 +0,0 @@
-# Create a script that reads the user name and shows a greetings message according to the entered value.
-name = input('What is your name? ')
-
-print('Greetings {}, nice to meet you!'.format(name))
