13743304dd54065612d66699acaead1628afc659
Veidz <joaovictorveidz@gmail.com>
2022-06-16 21:36:36 -0300

    refactor: exercise 02

--- exercises/exercise02.py
+++ exercises/exercise02.py
@@ -1,4 +1,4 @@
 # Create a script that reads the user name and shows a greetings message according to the entered value.
 name = input('What is your name? ')
 
-print('Greetings', name + ',', 'nice to meet you!')
+print('Greetings {}, nice to meet you!'.format(name))
