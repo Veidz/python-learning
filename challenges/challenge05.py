cab3444cf31f80892e2bf70091fdd79b504eba8e
Veidz <joaovictorveidz@gmail.com>
2022-06-19 01:55:08 -0300

    feat: challenge 05

---
+++ challenges/challenge05.py
@@ -0,0 +1,7 @@
+# Create a script that reads an integer number and shows its successor and its predecessor. 
+num = int(input('Type a number: '))
+
+successor = num + 1
+predecessor = num - 1
+
+print('Number: {}, Successor: {}, Predecessor: {}'.format(num, successor, predecessor))
