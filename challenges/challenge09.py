d1555f7a90965783d68632333b760856fe7ad2b2
Veidz <joaovictorveidz@gmail.com>
2022-06-19 02:26:55 -0300

    feat: challenge 09

---
+++ challenges/challenge09.py
@@ -0,0 +1,17 @@
+# Create a script that reads a number and show its multiplication table.
+print('Manipulation table\n')
+
+num = int(input('Type a number: '))
+print('=' * 50)
+
+print('{} x {}: {}'.format(num, 1, num * 1))
+print('{} x {}: {}'.format(num, 2, num * 2))
+print('{} x {}: {}'.format(num, 3, num * 3))
+print('{} x {}: {}'.format(num, 4, num * 4))
+print('{} x {}: {}'.format(num, 5, num * 5))
+print('{} x {}: {}'.format(num, 6, num * 6))
+print('{} x {}: {}'.format(num, 7, num * 7))
+print('{} x {}: {}'.format(num, 8, num * 8))
+print('{} x {}: {}'.format(num, 9, num * 9))
+print('{} x {}: {}'.format(num, 10, num * 10))
+print('=' * 50)
