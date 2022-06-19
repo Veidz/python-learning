e8c6c2d03020678fca4daeb39e454466b5ecb6d1
Veidz <joaovictorveidz@gmail.com>
2022-06-19 16:36:15 -0300

    feat: challenge 14

---
+++ challenges/challenge14.py
@@ -0,0 +1,6 @@
+# Create a script that converts a temperature from Celsius to Fahrenheit.
+temp_c = float(input('Enter a temperature (C): '))
+
+c_to_f = (temp_c * 9/5) + 32
+
+print(f'Celsius: {temp_c}\nFahrenheit: {c_to_f}')
