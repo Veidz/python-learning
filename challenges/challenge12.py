e00cbf7456671aef2ad8e11d6bfbbffa26be27ac
Veidz <joaovictorveidz@gmail.com>
2022-06-19 02:50:03 -0300

    refactor: challenge 12

--- challenges/challenge12.py
+++ challenges/challenge12.py
@@ -1,6 +1,7 @@
 # Create a script that reads a product price and shows it's new price with 5% off.
 price = float(input('Enter the product price: '))
 
-new_price = price * 0.95
+discount = 0.05
+new_price = price * (1 - discount)
 
 print('Product price (5% OFF): {}'.format(new_price))
