a077f7c7d9a217f1a53374b2d0e21f39a9373c45
Veidz <joaovictorveidz@gmail.com>
2022-06-19 02:06:35 -0300

    feat: challenge 07

---
+++ challenges/challenge07.py
@@ -0,0 +1,7 @@
+# Create a script that reads two grades from a student and show its average.
+grade1 = float(input('Enter your first grade: '))
+grade2 = float(input('Enter your second grade: '))
+
+average = (grade1 + grade2) / 2
+
+print('Average grade: {}'.format(average))
